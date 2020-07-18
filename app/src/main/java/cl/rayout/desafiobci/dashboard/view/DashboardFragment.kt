33b0b9b4433c5d2097732be1640f55c7f45779c2
Felipe Valdes <fvaldes.verdugo@gmail.com>
2020-07-18 14:44:49 -0400

    generic recycler view implemented

---
+++ app/src/main/java/cl/rayout/desafiobci/dashboard/view/DashboardFragment.kt
@@ -0,0 +1,32 @@
+package cl.rayout.desafiobci.dashboard.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import cl.rayout.desafiobci.R
+
+class DashboardFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = DashboardFragment()
+    }
+
+    private lateinit var viewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dashboard_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/cl/rayout/desafiobci/dashboard/view/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package cl.rayout.desafiobci.dashboard.view
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
